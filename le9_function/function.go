474ab5f5ab2f4496d15dd9e72a71f8a7fde3ca5f
gtyi999 <gtyi999@163.com>
2017-03-21 18:12:29 +0800

    function

--- le9_function/function.go
+++ le9_function/function.go
@@ -27,6 +27,65 @@ func main(){
 	fmt.Println(s1)
 	fmt.Println("2--function slice值传递 end")
 
+	fmt.Println("3--function 指针传递 start")
+	aa:=1
+	G(&aa)
+	fmt.Println(aa)
+	fmt.Println("3--function 指针传递 end")
+
+	fmt.Println("4--函数类型 start")
+	funcH:=H
+	funcH()
+	fmt.Println("4--函数类型 end")
+
+	fmt.Println("5--匿名函数 start")
+	funcI:=func(){
+		fmt.Println("func I")
+	}
+	funcI()
+	fmt.Println("5--匿名函数 start")
+
+	fmt.Println("6--闭包函数 start")
+	funcJ:=closure(10)
+	fmt.Println(funcJ(1))
+	fmt.Println(funcJ(2))
+	fmt.Println("6--闭包函数 end")
+
+	fmt.Println("7--defer 关键字 start")
+	//1-----
+	//fmt.Println("a")
+	//defer fmt.Println("b")
+	//defer fmt.Println("c")
+        //2---
+	//for ii:=0;ii<3;ii++{
+	//	defer fmt.Println(ii)
+	//}
+	//3--
+	//for qq:=0;qq<3;qq++{
+	//	defer func(){
+	//		fmt.Println(qq)
+	//	}()
+	//}
+	K()
+	L()
+	M()
+	fmt.Println("7--defer 关键字 end")
+
+	fmt.Println("8--课堂作业 start")
+	var fs=[4]func(){}
+	for jj:=0;jj<4;jj++{
+		defer fmt.Println("defer jj= ",jj)
+		defer func(){fmt.Println("defer_closure jj= ",jj)}()
+		fs[jj]=func(){fmt.Println("closure jj= ",jj)}
+	}
+	for _,f:=range fs{
+		f()
+	}
+	
+
+
+
+
 }
 
 func A(a int,b string) int {
@@ -53,3 +112,36 @@ func E(s []int){
 	s[3]=8
 }
 
+func G(a *int){
+	*a=2
+	fmt.Println(a)
+}
+func H(){
+	fmt.Println("func A")
+}
+
+func closure(x int) func(int) int{
+	fmt.Println("%p\n",&x)
+	return func(y int)int{
+		fmt.Println("%p\n",&x)
+		return x+y
+	}
+}
+
+func K(){
+	fmt.Println("Func K")
+
+}
+func L(){
+	defer func(){
+		if err:=recover();err!=nil{
+			fmt.Println("Recover in L")
+		}
+	}()
+	panic("Panic in L")
+
+}
+
+func M(){
+	fmt.Println("Func M")
+}
