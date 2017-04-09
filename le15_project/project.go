0d480dfe53d08a2a0878f1719b0f61ba13fd62d1
gtyi999 <gtyi999@163.com>
2017-04-09 21:59:57 +0800

    le15

---
+++ le15_project/project.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+var c chan string
+
+func Pingpong(){
+	i:=0
+	for{
+		fmt.Println(<-c)
+		c <- fmt.Sprintf("From Pingpong:Hi, #%d",i)
+		i++
+	}
+}
+func main(){
+        fmt.Println("1---课堂作业 start")
+	c=make(chan string)
+	go Pingpong()
+	for i:=0;i<10;i++{
+		c<- fmt.Sprintf("From main:Hello,#%d",i)
+		fmt.Println(<-c)
+	}
+	fmt.Println("1---课堂作业 end")
+
+
+}
